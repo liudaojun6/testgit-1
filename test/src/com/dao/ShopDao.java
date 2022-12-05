a6623878c7352c343eb2889d068968e7f8fb103f
chenxujie <1158189673@qq.com>
2022-12-05 18:28:33 +0800

    删除多余文件

--- test/src/com/dao/ShopDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.vo.Seller;
-import com.vo.Shop;
-import java.sql.SQLException;
-
-public interface ShopDao {
-  Shop fullshop(Seller paramSeller) throws SQLException;
-}
