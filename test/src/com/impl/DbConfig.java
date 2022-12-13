a8d14fcdd5aae13a2791635422199f65d2cf3ab4
ztc123-ok <2844692623@qq.com>
2022-12-13 21:42:46 +0800

    sqlite路径修改

--- test/src/com/impl/DbConfig.java
+++ test/src/com/impl/DbConfig.java
@@ -1,5 +1,7 @@
 package com.impl;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,12 +9,29 @@ import java.sql.SQLException;
 
 
 public class DbConfig {
+	
 	public static final String DRIVER = "org.sqlite.JDBC";
 	
-	public static final String URL = "jdbc:sqlite:C:/Users/周/Desktop/验收待完善(1)/验收待完善/test/test.db";
+	String URL = "jdbc:sqlite:";
 	File f = new File(this.getClass().getResource("../..").getPath());
+	String str=f.getAbsolutePath();
 	public Connection dbConfig() throws SQLException{
-		System.out.println(f);
+		try {
+			str=URLDecoder.decode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+
+		System.out.println(str);
+		str=str.replace("\\", "/");
+		for(int i=0;i<2;i++){
+			int index = str.lastIndexOf("/");
+			str = str.substring(0, index);
+		}
+		System.out.println(str);
+		str=str+"/db/test.db";
+		URL=URL+str;
+		System.out.println(URL);
 		try {
 			Class.forName(DRIVER);
 		} catch (ClassNotFoundException e) {
