49697a785bbe30c9db3268fa571b872d803dafe1
chenxujie <1158189673@qq.com>
2022-12-14 20:09:15 +0800

    修改

--- webapp/target/liberty/wlp/lib/platform/checksums/com.ibm.websphere.appserver.logging-1.0.cs
+++
@@ -1,7 +0,0 @@
-#Thu Aug 20 11:43:50 BST 2020
-lib/com.ibm.ws.collector.manager_1.0.44.jar=cf9cd43cf100996d8c24faf055ac70c8
-lib/platform/defaultLogging-1.0.mf=5dca00ad3a1c72fd06670dc28717f34f
-dev/spi/ibm/javadoc/com.ibm.websphere.appserver.spi.logging_1.1-javadoc.zip=a0ca78616806480accbb35219a286b61
-lib/com.ibm.ws.logging.osgi_1.0.44.jar=d3bc5720d8b1eabc8bda41700b9d9724
-dev/spi/ibm/com.ibm.websphere.appserver.spi.logging_1.1.44.jar=9cb40903c2ed20377b9c6a4b5bc3ffb7
-lib/com.ibm.ws.logging_1.0.44.jar=10a6effbdd4747621ce939d1faab783c
